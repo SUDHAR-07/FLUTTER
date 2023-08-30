c3622b425e0862284038660720612d67c330e091
SUDHAR-07 <85281859+SUDHAR-07@users.noreply.github.com>
2023-08-30 19:20:29 +0530

    Add files via upload

--- lib/main.dart
+++ lib/main.dart
@@ -1,30 +1,93 @@
-// import 'dart:html';
+import 'dart:math';
 
 import 'package:flutter/material.dart';
 
-// void main() {
-//   runApp(const MaterialApp(
-//       home: Center(
-//     child: Text('HELLO WORLD'),
-//   )));
-// }
-
 void main() {
-  runApp(MaterialApp(
-    home: Scaffold(
-      backgroundColor: Colors.amber[100],
-      appBar: AppBar(
-        title: const Text('I AM RICH'),
-        backgroundColor: Colors.black87,
-        // toolbarTextStyle: const TextStyle(color: Colors.yellowAccent),
-        // titleTextStyle: const HSVColor.fromColor(Colors.black87),
-      ),
-      body: const Center(
-        child: Image(
-          image: AssetImage('images/IMG_8954.JPG'),
-          // image: NetworkImage('https://pixlr.com/images/index/remove-bg.webp'),
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.black,
+        appBar: AppBar(
+          title: const Text(
+            'Dicee',
+            // textAlign: TextAlign.center,
+            // textDirection: ,
+            style: TextStyle(
+              color: Colors.black87,
+            ),
+          ),
+          centerTitle: true,
+          backgroundColor: Colors.yellowAccent,
         ),
+        body: const Dice(),
       ),
     ),
-  ));
+  );
+}
+
+class Dice extends StatefulWidget {
+  const Dice({super.key});
+
+  @override
+  State<Dice> createState() => _DiceState();
+}
+
+class _DiceState extends State<Dice> {
+  int leftdiceno = 1, right = 1;
+  void getDice() {
+    leftdiceno = Random().nextInt(6) + 1;
+    right = Random().nextInt(6) + 1;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Row(
+        // mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Expanded(
+            flex: 1,
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: TextButton(
+                onPressed: () {
+                  setState(() {
+                    getDice();
+                  });
+                },
+                child: Image(
+                  image: AssetImage("IMAGE/dice$leftdiceno.png"),
+                  color: Colors.yellowAccent,
+                ),
+              ),
+            ),
+          ),
+          Expanded(
+            flex: 1,
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: TextButton(
+                onPressed: () {
+                  setState(() {
+                    // if (right > 5) {
+                    //   right = 1;
+                    // } else {
+                    //   right++;
+                    // }
+                    getDice();
+                  });
+                },
+                child: Image(
+                  image: AssetImage("IMAGE/dice$right.png"),
+                  // height: 120.0,
+                  // width: 200.0,
+                  color: Colors.yellowAccent,
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
 }
